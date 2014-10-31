afbaa70dc62ccb357488a1ac97a666b143ee43e0
danielobembe <ayo.obembe@gmail.com>
2014-10-31 15:29:39 +0000

    passenger.initialize; passenger.checked_in?; associated tests

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,4 +1,15 @@
 class Passenger
 
+	def initialize(name, account)
+		@name = name
+		@account = account
+		@check_in_status = false
+		@origin
+		@destination
+	end
+
+	def checked_in?
+		@check_in_status
+	end
 
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,7 +1,22 @@
 require "passenger"
 
 describe Passenger do 
+	let(:passenger){Passenger.new("Ayo",100)}
+
+	context "Object self-awareness tests"
+		
+		it "should exist when instantiated" do
+			expect(passenger.nil?).to be false
+		end
+
+		it "should know if it's checked in" do
+			expect(passenger.checked_in?). to be false
+		end
+
+		
 
 
 
+
+	
 end
